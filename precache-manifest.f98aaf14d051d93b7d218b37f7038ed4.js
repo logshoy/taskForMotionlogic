4ff30ed61bd4c87c69e4bee71bae89d637eecf68
logshoy <logshoy@gmail.com>
2020-07-12 14:59:38 +0500

    Updates

---
+++ precache-manifest.f98aaf14d051d93b7d218b37f7038ed4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1d62444eb512c88053924288af0d211",
+    "url": "/taskForMotionlogic/index.html"
+  },
+  {
+    "revision": "e04a6af784b4061ee59f",
+    "url": "/taskForMotionlogic/static/css/main.24252004.chunk.css"
+  },
+  {
+    "revision": "59916f2660046673d0b2",
+    "url": "/taskForMotionlogic/static/js/2.c8b29b13.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/taskForMotionlogic/static/js/2.c8b29b13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e04a6af784b4061ee59f",
+    "url": "/taskForMotionlogic/static/js/main.4e887c44.chunk.js"
+  },
+  {
+    "revision": "b70505af9248d91f7f68",
+    "url": "/taskForMotionlogic/static/js/runtime-main.e121f0ac.js"
+  }
+]);
